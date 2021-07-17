8fe41abb4902056310d053c7aebbddf49ebb56ee
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 20:25:54 +0400

    uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -3,28 +3,28 @@
 import os
 
 with open("/data/data/com.termux/files/home/.bashrc", "r") as f:
-    raw = f.read().lower().replace("alias sudo='python3 pass.py && sudo'", " ")
+    raw = f.read().lower().replace("alias sudo='python3 ~/sudopass/pass.py && sudo'", " ")
     file = open("/data/data/com.termux/files/home/.bashrc", "w")
     file.write(raw)
     file.close()
     f.close()
 
 with open("/data/data/com.termux/files/home/.bashrc", "r") as f:
-    raw = f.read().lower().replace("alias sudo='sudo python3 pass.py && sudo", " ")
+    raw = f.read().lower().replace("alias sudo='sudo python3 ~/sudopass/pass.py && sudo", " ")
     file = open("/data/data/com.termux/files/home/.bashrc", "w")
     file.write(raw)
     file.close()
     f.close()
 
 with open("/data/data/com.termux/files/home/.zshrc", "r") as f:
-    raw = f.read().lower().replace("alias sudo='python3 pass.py && sudo'", " ")
+    raw = f.read().lower().replace("alias sudo='python3 ~/sudopass/pass.py && sudo'", " ")
     file = open("/data/data/com.termux/files/home/.zshrc", "w")
     file.write(raw)
     file.close()
     f.close()
 
 with open("/data/data/com.termux/files/home/.zshrc", "r") as f:
-    raw = f.read().lower().replace("alias sudo='sudo python3 pass.py && sudo", " ")
+    raw = f.read().lower().replace("alias sudo='sudo python3 ~/sudopass/pass.py && sudo", " ")
     file = open("/data/data/com.termux/files/home/.zshrc", "w")
     file.write(raw)
     file.close()
