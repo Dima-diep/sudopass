70bef9171127ed99345169008465c0f792fd9ee7
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 17:19:14 +0400

    Delete chpass.rb

--- chpass.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#! -*- coding: utf-8 -*-
-
-with open("/data/data/com.termux/files/home/sudopass/pass.py", "r") as f:
-    raw = f.read().lower().replace("oldpass", "newpass")
-    file = open("source.txt", "w")
-    file.write(raw)
-    file.close()
-    f.close()
