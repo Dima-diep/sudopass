a6f2ff30b1acf52332e5b70310db95705c6fedc1
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 17:19:39 +0400

    chpass.py

---
+++ chpass.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+
+with open("/data/data/com.termux/files/home/sudopass/pass.py", "r") as f:
+    raw = f.read().lower().replace("oldpass", "newpass")
+    file = open("source.txt", "w")
+    file.write(raw)
+    file.close()
+    f.close()
