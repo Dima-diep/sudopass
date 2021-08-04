d0560b9ced5d7246bfae8288b480638d70672303
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-04 20:41:51 +0400

    unadd.sh

--- unadd.sh
+++ unadd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo chattr -i ~/sudopass/chpass.rb
+sudo chattr -i ~/sudopass/chpass.py
 sudo chattr -i ~/sudopass/pass.py
-sudo chmod 700 ~/sudopass/chpass.rb
+sudo chmod 700 ~/sudopass/chpass.py
 sudo chmod 700 ~/sudopass/pass.py
