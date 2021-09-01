4b808e8134aeac1110f1b396fd1e4f83af3283a0
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-09-01 10:32:06 +0400

    install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ echo "alias tsu='python3 ~/sudopass/pass.py && tsu'" >> ~/.bashrc
 echo "alias tsu='python3 ~/sudopass/pass.py && tsu'" >> ~/.zshrc
 echo "alias su='python3 ~/sudopass/pass.py && su'" >> ~/.bashrc
 echo "alias su='python3 ~/sudopass/pass.py && su'" >> ~/.zshrc
+source ~/.bashrc
+source ~/.zshrc
